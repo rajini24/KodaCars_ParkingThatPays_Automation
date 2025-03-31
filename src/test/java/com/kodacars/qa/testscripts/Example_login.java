b7c533064da783e4246f3e64109cf36cda5af8d5
Bha001 <bhavyaiyer1@gmail.com>
2025-03-31 18:03:39 -0500

    added some code

---
+++ src/test/java/com/kodacars/qa/testscripts/Example_login.java
@@ -0,0 +1,14 @@
+package com.kodacars.qa.testscripts;
+
+import org.openqa.selenium.By;
+
+public class Example_login {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Clicked Successfully");
+	
+
+	}
+
+}
